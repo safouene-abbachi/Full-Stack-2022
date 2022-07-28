1b333486754bc519778cc7dd5066933b00282287
safouene <safouene-abbachi@hotmail.fr>
2022-07-28 17:27:27 +0100

    DEV : finishing the phonebook part

--- part02/phonebook/src/App.js
+++ part02/phonebook/src/App.js
@@ -1,14 +1,10 @@
-import { useState, useMemo } from 'react';
+import { useState, useMemo, useEffect } from 'react';
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
+import axios from 'axios';
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [searchField, setSearchField] = useState('');
@@ -47,6 +43,13 @@ const App = () => {
   const handleSearch = (e) => {
     setSearchField(e.target.value);
   };
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons').then((response) => {
+      setPersons(response.data);
+    });
+  }, []);
+
   return (
     <div>
       <h2>Phonebook</h2>
