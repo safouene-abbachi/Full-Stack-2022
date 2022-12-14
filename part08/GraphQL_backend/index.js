5ab9bfcae7449217029db06f7a303f64b6ed33b3
safouene <safouene-abbachi@hotmail.fr>
2022-12-14 16:48:12 +0100

    Adding 8.4: Books of an author

--- part08/GraphQL_backend/index.js
+++ part08/GraphQL_backend/index.js
@@ -105,7 +105,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Books!]!
+    allBooks(author: String): [Books!]!
     allAuthors: [Authors!]!
   }
 `;
@@ -114,7 +114,11 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      const booksByAuthor = books.filter((book) => book.author === args.author);
+      console.log('🚀 ~ booksByAuthor', booksByAuthor);
+      return booksByAuthor;
+    },
     allAuthors: () => authors,
   },
   Authors: {
